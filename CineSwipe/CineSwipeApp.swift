b80dbc8d83bfcf514124b8f10772642c8a1a41cd
HaiZen <haizendev@outlook.fr>
2026-05-22 11:08:36 +0200

    Initial Commit

---
+++ CineSwipe/CineSwipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CineSwipeApp.swift
+//  CineSwipe
+//
+//  Created by Alan on 22/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct CineSwipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
